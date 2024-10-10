c6a9db145aed83f64d951750bd3e56c4fbb85525
mallengoebel <matt.allengoebel@jbhunt.com>
2024-10-10 11:18:06 -0500

    Anonymizing wac-import-assets-sequestered-instance.ps1

--- wac-import-assets-sequestered-instance.ps1
+++ wac-import-assets-sequestered-instance.ps1
@@ -1,37 +1,15 @@
 # this script is to collect, from AD and VMM, the Windows Servers list and clusters list
-# the lists can be imported into Windows Admin Center, under 'shared' connections
-# created 2/23/2021 by Kendall Conner
-# maintainer - Matt Allen-Goebel 
-# forked and renamed													09/20/2024	malgoebel	v.1.1
-# added 2k22Servers vars												09/20/2024	malgoebel	v.1.11
-# added under construction section										09/20/2024	malgoebel	v.1.12
-# adding logging and content variables/path validation and creation		09/26/2024	malgoebel	v.1.2
-# refactor to append to one uber content file 							09/26/2024	malgoebel	v.2.0
-# update to CSV file format and headings structure						09/26/2024	malgoebel	v.2.01
-# breaking out logging path to separate from delagation task logs		10/01/2024	malgoebel	v.2.11
-# adding clusters and vmhosts sections									10/01/2024	malgoebel	v.2.51
-# adding opening content csv file with headers							10/01/2024	malgoebel	v.2.61
-# log file garbage collection sections									10/01/2024	malgoebel	v.2.71
-# Removed under construction sections									10/01/2024	malgoebel	v.2.75
-# Added try/catch block to retry content additions/avoid stream error	10/02/2024	malgoebel	v.2.81
-# removed redundant variables 											10/02/2024	malgoebel	v.2.85
-# adding import task section with new logging (renewed vars)			10/02/2024	malgoebel	v.2.95
-# Forking and creating domain specific instance							10/03/2024	malgoebel	v.3.0
-# adding VM Host discovery and tagging									10/04/2024	malgoebel	v.3.11
-# common Error Handling for tagging										10/08/2024	malgoebel	v.3.51
-
-
 
 # Global Vars
 $DomainName = (Get-WmiObject Win32_ComputerSystem).Domain
 $WacUrl = "https://wac.$DomainName"
 
 # Content Vars
-$contentDirectory = "C:\nt\code\Content\"
+$contentDirectory = "C:\path\to\Content\"
 $contentFileName = "wac-HostsList.csv"
 
 # Logging Vars
-$logDirectory = "C:\nt\code\Logs\build\"
+$logDirectory = "C:\path\to\Logs\build\"
 $logFileName = "wac-build-csv_{0}.log" -f (Get-Date -Format "yyyyMMddhhmmss")
 
 # Create the log directory if it doesn't exist
@@ -54,7 +32,6 @@ $contentFilePath = Join-Path -Path $contentDirectory -ChildPath $contentFileName
 $logEntry = "{0} - Opening the log. This log is for tracking failures to connect. " -f (Get-Date -Format "yyyy-MM-dd HH:mm:ss")
 Add-Content -Path $logFilePath -Value $logEntry
 
-
 # Garbage Collection 
 Write-Output "Garbage Collection from $contentDirectory"
 Remove-Item -path $contentDirectory\*.*
@@ -69,7 +46,7 @@ Add-Content -Path $logFilePath -Value $logEntry
 Write-Host "Working on $DomainName Hosts"
 	
 # Define the group objects to work on
-$Computer = (Get-ADComputer -Server $DomainName -Filter 'OperatingSystem -NotLike "*Server 2008*"' | Sort-Object Name).Name | findstr /i "jbv jvl jvd jvt jvp"
+$Computer = (Get-ADComputer -Server $DomainName -Filter 'OperatingSystem -NotLike "*Server 2008*"' | Sort-Object Name).Name | findstr /i "your variables to search against"
     
 # Note which Group and domain
 $Count = ($Computer).Count
@@ -78,12 +55,16 @@ Add-Content -Path $logFilePath -Value $logEntry
 
 # Loop on the items in Computers
 foreach ($item in $Computer) {
+
+ 	# Various Connection tests and off ramps 
     if (Test-Connection -ComputerName $item -Count 1 -Quiet) {
         if (Test-NetConnection -ComputerName $item -Port 22 -InformationLevel Quiet -WarningAction SilentlyContinue) {
             $logEntry = "{0} - ${item} has passed an SSH open port check. This is likely a Linux Server. Skipping." -f (Get-Date -Format "yyyy-MM-dd HH:mm:ss")
             Add-Content -Path $logFilePath -Value $logEntry
         } else {
             $vmhostname = $null
+
+    		# Error logging
             try {
                 if (Invoke-Command -ComputerName $item { Test-Path -Path "HKLM:\SOFTWARE\Microsoft\Virtual Machine\Guest\Parameters" }) {
                     $vmhostname = Invoke-Command -ComputerName $item {
@@ -112,11 +93,11 @@ foreach ($item in $Computer) {
 }
 
 # Get the Cluster Server Names
-$jbhvmhostcluster = (Get-SCVMHostCluster -VMMServer "scvmmconsole.$DomainName" | Sort-Object Name).ClusterName
-Write-Output "Clusters in ${DomainName}: $($jbhvmhostcluster.Count)"
+$vmhostcluster = (Get-SCVMHostCluster -VMMServer "yourscvmmconsole.$DomainName" | Sort-Object Name).ClusterName
+Write-Output "Clusters in ${DomainName}: $($vmhostcluster.Count)"
 
 # Set the Static Domain
-$domain = "JBH01"
+$domain = "yourdomain" # ( required if you run a multi domain environment and your clusters/vmhosts are in one domain and have guests in another domain.
 
 # Function to test connection and log results
 function Test-ConnectionAndLog {
@@ -135,16 +116,16 @@ function Test-ConnectionAndLog {
 }
 
 # Loop through each Cluster Server
-foreach ($item in $jbhvmhostcluster) {
+foreach ($item in $vmhostcluster) {
     Test-ConnectionAndLog -computerName $item -connectionType "cluster"
 }
 
 # Get the Host Server Names
-$jbhvmhosts = (Get-SCVMHost -VMMServer "scvmmconsole.$DomainName" | Sort-Object ComputerName).ComputerName
-Write-Output "VM Hosts in ${DomainName}: $($jbhvmhosts.Count)"
+$vmhosts = (Get-SCVMHost -VMMServer "yourscvmmconsole.$DomainName" | Sort-Object ComputerName).ComputerName
+Write-Output "VM Hosts in ${DomainName}: $($vmhosts.Count)"
 
 # Loop through each Host Server
-foreach ($item in $jbhvmhosts) {
+foreach ($item in $vmhosts) {
     Test-ConnectionAndLog -computerName $item -connectionType "server"
 }
 
@@ -160,7 +141,7 @@ foreach ($file in $filesToDelete) {
 # Import task section
 
 # Renewing Logging Vars
-$logDirectory = "C:\nt\code\Logs\Import\"
+$logDirectory = "C:\path\to\Logs\Import\"
 $logFileName = "wac-import_{0}.log" -f (Get-Date -Format "yyyyMMddhhmmss")
 
 # Create the log directory if it doesn't exist
